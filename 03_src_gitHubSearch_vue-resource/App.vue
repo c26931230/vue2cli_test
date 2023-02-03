4c205497a7f48f9ae36052d8caafaefe01eebb9a
Mandy_Li <c26931230@gmail.com>
2023-02-03 15:21:05 +0800

    gitHub 搜尋 vue-source

---
+++ 03_src_gitHubSearch_vue-resource/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <searchBox/>
+    <userList/>
+  </div>
+</template>
+<script>
+// import axios from 'axios'
+import searchBox from './components/searchBox.vue';
+import userList from './components/userList.vue';
+export default{
+  name:'App',
+  components:{searchBox,userList}
+} 
+</script>
+<style lang="scss">
+@import "assets/scss/main.scss";
+@import '~bootstrap/scss/bootstrap';  
+</style>
