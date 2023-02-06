1d64c18e7ef91c5b3fb81fea8e520c02e622a4f0
Mandy_Li <c26931230@gmail.com>
2023-02-06 14:17:01 +0800

    路由的query

---
+++ 10_src_路由的query/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue Router Demo</h2></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-2">
+        <div class="list-group">
+          <router-link to="/about" class="list-group-item" active-class="active">About</router-link>
+          <router-link to="/home" class="list-group-item" active-class="active">Home</router-link>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="panel">
+          <div class="panel-body">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap';  
+</style>
---
+++ src/pages/MyDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>detail</h1>
+        <p>{{ $route.query.id }}</p>
+        <p>{{ $route.query.title }}</p>
+    </div>
+</template>
+<script>
+export default{
+    name:'MyDetail',
+    mounted() {
+        console.log(this.$route);
+    },
+}
+</script>
+<style lang="scss">
+
+</style>
